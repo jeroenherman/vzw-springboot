e7824135442ad3c9e5973667eba9b7815ad9eda4
jeroen herman <jeroen.herman@outlook.be>
2019-03-16 09:42:25 +0100

    changed fixed urls for war deployment

--- src/main/java/be/voedsaam/vzw/VzwSpringbootApplication.java
+++ src/main/java/be/voedsaam/vzw/VzwSpringbootApplication.java
@@ -2,13 +2,28 @@ package be.voedsaam.vzw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class VzwSpringbootApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(VzwSpringbootApplication.class, args);
+//	}
+//
+//}
 
 @SpringBootApplication
-public class VzwSpringbootApplication {
+public class VzwSpringbootApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VzwSpringbootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VzwSpringbootApplication.class, args);
 	}
 
 }
-
