f132a304fee8d5b0ad10ce016500892f88ea7e40
jeroen Herman <jeroen.herman@outlook.be>
2019-05-28 20:11:18 +0200

    website beta2

--- src/main/java/be/voedsaam/vzw/service/mapper/StockMapper.java
+++ src/main/java/be/voedsaam/vzw/service/mapper/StockMapper.java
@@ -37,7 +37,7 @@ public class StockMapper extends AbstractMapper<Stock, StockDTO> {
            d.setNoUsers(false);
        else
            d.setNoUsers(true);
-        
+
        return  d;
     }
 
